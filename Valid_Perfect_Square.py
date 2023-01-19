f4972a19b9eae1fc0fd1d4c049285f087312dc5a
Psowjanya-03 <86092446+Psowjanya-03@users.noreply.github.com>
2023-01-19 14:26:19 +0530

    t=int(input()) for i in range(t):     n=int(input())     c=n**0.5     k=int(c)     if c==k:         print(True)     else:         print(False)

--- Valid_Perfect_Square.py
+++ Valid_Perfect_Square.py
@@ -1,10 +1,9 @@
 t=int(input())
-for i in range (t):
+for i in range(t):
     n=int(input())
-    c = n**0.5
+    c=n**0.5
     k=int(c)
     if c==k:
         print(True)
     else:
         print(False)
-        
